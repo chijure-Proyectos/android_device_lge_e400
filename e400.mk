fb1bfe80be851c09e853cbedaa8c1f88666069c0
jhuan Reategui <chijure100x100to@gmail.com>
2018-01-08 21:25:33 -0500

    Fix On Signet-off-by: Jhuan Reategui <chijure100x100to@gmail.com>

--- e400.mk
+++ e400.mk
@@ -57,6 +57,9 @@ PRODUCT_COPY_FILES += \
 # BT
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/chgchk:system/bin/chgchk
 	
 # Baseband
 PRODUCT_PACKAGES += \
